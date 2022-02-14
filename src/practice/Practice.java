95366caf51b4055ca45e2c95ee20082af24f87ee
Dakshil99 <97900384+Dakshil99@users.noreply.github.com>
2022-02-14 06:18:58 -0500

    Update Practice.java

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -21,6 +21,7 @@ public class Practice {
     public static void main(String[] args) {
        
         System.out.println("Learning Github");
+        System.out.println("new code");
     }
     
 }
